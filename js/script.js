2bd797128899f340dacf596114ea45161c9effd4
Anlluly <aterannaraine@gmail.com>
2023-05-21 15:49:53 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -11,8 +11,9 @@ function generatePDF(){
         filename:     `${invoice}.pdf`,
         image:        { type: 'jpeg', quality: 0.98 },
         html2canvas:  { scale: 2 },
-        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        jsPDF:        { unit: 'in', format: 'A4', orientation: 'portrait' }
       };
 
       html2pdf().set(opt).from(element).save();
+      
 }
